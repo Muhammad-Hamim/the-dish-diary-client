ccd36da6f2ea268e634c36a54ce77ee58caf91a4
Muhammad-Hamim <hamim.ml01@gmail.com>
2023-05-05 06:46:30 +0600

    created env file to hide firebase secret key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhhnfiQpMTMRS8ITbTTSaLtVNtWdlx99Q",
-  authDomain: "the-dish-diary-auth-3421e.firebaseapp.com",
-  projectId: "the-dish-diary-auth-3421e",
-  storageBucket: "the-dish-diary-auth-3421e.appspot.com",
-  messagingSenderId: "1031850032565",
-  appId: "1:1031850032565:web:1f2bc152908cb8beaa2dfc",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
